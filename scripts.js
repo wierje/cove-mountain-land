4c7e01627ef057a5be482611ad430383d05ff51b
wierje <wier.je@gmail.com>
2017-02-10 09:47:33 -0600

    added styling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var mapCanvas = document.getElementById('map');
 var mapOptions ={
   center: new google.maps.LatLng(35.716, -83.609), zoom: 10
-  }
+  };
   var map = new google.maps.Map(mapCanvas, mapOptions);
